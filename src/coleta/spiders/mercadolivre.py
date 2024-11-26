e3c225997018f8a8e3d9030cf47262acc7d91b62
Genivaldo <genivaldo.serra@live.com>
2024-11-26 10:47:39 -0300

    feat: Coletando informações dos produtos

--- src/coleta/spiders/mercadolivre.py
+++ src/coleta/spiders/mercadolivre.py
@@ -7,4 +7,13 @@ class MercadolivreSpider(scrapy.Spider):
     start_urls = ["https://lista.mercadolivre.com.br/informatica/portateis-acessorios/_Container_black-friday-f1-ce-games-e-notebooks#applied_filter_id%3Dcategory%26applied_filter_name%3DCategorias%26applied_filter_order%3D4%26applied_value_id%3DMLB430687%26applied_value_name%3DPort%C3%A1teis+e+Acess%C3%B3rios%26applied_value_order%3D7%26applied_value_results%3D212%26is_custom%3Dfalse"]
 
     def parse(self, response):
-        pass
+        products = response.css('div.ui-search-result__wrapper')
+        
+        for product in products:
+            yield {
+                'name': product.css('a::text').get(),
+                'old_price': product.css('span.andes-money-amount__fraction::text').get(),
+                'new_price': product.css('span.andes-money-amount__fraction::text').get(),
+                'reviews_rating': product.css('span.poly-reviews__rating::text').get(),
+                'reviews_total': product.css('span.poly-reviews__total::text').get()
+            }
