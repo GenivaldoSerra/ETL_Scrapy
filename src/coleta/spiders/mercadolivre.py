6236e1499cc0b309220e939c3689219033e831ca
Genivaldo <genivaldo.serra@live.com>
2024-11-27 08:39:04 -0300

    feat: Incluindo a funcionalidade de coleta em multiplas paginas

--- src/coleta/spiders/mercadolivre.py
+++ src/coleta/spiders/mercadolivre.py
@@ -1,10 +1,12 @@
 import scrapy
+import scrapy.resolver
 
 
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/informatica/portateis-acessorios/_Container_black-friday-f1-ce-games-e-notebooks#applied_filter_id%3Dcategory%26applied_filter_name%3DCategorias%26applied_filter_order%3D4%26applied_value_id%3DMLB430687%26applied_value_name%3DPort%C3%A1teis+e+Acess%C3%B3rios%26applied_value_order%3D7%26applied_value_results%3D212%26is_custom%3Dfalse"]
+
 
     def parse(self, response):
         products = response.css('div.ui-search-result__wrapper')
@@ -22,3 +24,7 @@ class MercadolivreSpider(scrapy.Spider):
                 'reviews_rating': product.css('span.poly-reviews__rating::text').get(),
                 'reviews_total': product.css('span.poly-reviews__total::text').get()
             }
+            
+        next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+        yield scrapy.Request(url=next_page, callback=self.parse)
+        
